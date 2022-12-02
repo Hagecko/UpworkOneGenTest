312bf3672c0184733a7ab63d412a5a3cbb85e8fc
Hagecko <102910178+Hagecko@users.noreply.github.com>
2022-12-02 13:44:50 +0100

    Add files via upload

---
+++ Author.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct Author: Codable {
+    var name: String
+    var age: Int
+    var adress: String
+    var currentlyWriting: Bool
+}
---
+++ Books.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+struct Books: Codable {
+    var id: Int64
+    var name: String
+    var year: Int
+}
